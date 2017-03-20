dd26fbbcfc355e8928bb9491d184a86434b4d5a8
Gabriel Morais Silva <g.moraissilva@outlook.com>
2017-03-20 12:23:12 -0300

    Aplicação da Goople Maps API:  - Uso do mapa estilizado.  - Aplicação do marcador.  - Aplicação do launcher logo.

---
+++ app/src/main/java/com/sudo/trampaki/MapsActivity.java
@@ -0,0 +1,35 @@
+package com.sudo.trampaki;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(this,R.raw.map_style));
+        LatLng myHome = new LatLng(-23.9531824,-46.3948659);
+        mMap.addMarker(new MarkerOptions().position(myHome).icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_marker)));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(myHome,15));
+    }
+}
